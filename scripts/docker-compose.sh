37d171caff93cb3b93d7e3b159a09d3f72407aa8
Loïc GUEZO <loicguezo@gmail.com>
2025-07-31 15:15:32 +0200

    fix: docker command

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -3,14 +3,14 @@
 INSTALLED=$1
 if [[ "--install" == $INSTALLED ]]; then 
     source /usr/local/bin/libs/common.sh
-    
+
     info_print "\n\
 ==================================================\n\
             docker-compose Installation\n\
 --------------------------------------------------"
     
     if [[ -f "$ETC_DIR/docker-compose.yml" ]]; then
-        docker compose up -d -f $ETC_DIR/docker-compose.yml && \
+        docker compose -f "$ETC_DIR/docker-compose.yml" up -d && \
         info_print "$ETC_DIR/docker-compose.yml is running." 2;
 
     else info_print "no docker-compose.yml found at $ETC_DIR" 6;
